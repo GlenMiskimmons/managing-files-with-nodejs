2bb990b632f71303be2dde96bac3c354ab319415
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-02-04 20:34:33 +0000

    Module 04 Writing to a File
    Utilizing Options

--- asynchronous.write.js
+++ asynchronous.write.js
@@ -1,7 +1,8 @@
-const { writeFile } = require('fs');
+const { constants, writeFile } = require('fs');
 
-writeFile('./data/app.log',
+writeFile('./data/newapp.log',
   '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512',
+  { encoding: 'base64' },
   (err) => {
     err ? console.log(err) : console.log("file saved!");
   });
