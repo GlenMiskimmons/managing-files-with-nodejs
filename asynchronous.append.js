81fe026f680b3ebe73de0153899c2a561fc8004a
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-02-04 20:23:24 +0000

    Module 04 Writing to a File
    Appending to a File

--- asynchronous.append.js
+++ asynchronous.append.js
@@ -1,6 +1,6 @@
-const { writeFile } = require('fs');
+const { appendFile } = require('fs');
 
-writeFile('./data/app.log',
+appendFile('./data/app.log',
   '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512',
   (err) => {
     err ? console.log(err) : console.log("file saved!");
