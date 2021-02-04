aaa80f018f39ffed64abc3f827e9cfe014b2c0ef
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-02-04 19:24:43 +0000

    Module 02 Reading an Entire File
    Reading a File Synchronously

---
+++ synchronous.read.js
@@ -0,0 +1,10 @@
+const { convertCsv } = require('./csv.parse');
+const { readFileSync } = require('fs');
+
+try {
+  const data = readFileSync("./data/pulitzer-circulation-data.csv", "utf-8");
+
+  console.table(convertCsv(data));
+} catch (error) {
+  console.log(`There was a problem with the file ${err}`);
+}
